1115f60f1b1aa4ef759668d504730a437460d799
tinepapa <tinembaye@hotmail.com>
2022-08-15 12:29:35 +0000

    uploading deployment_screenshots

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": "udagramtinedev",
+    "password": "udagramtine22",
+    "database": "udagramtinedev",
+    "host": "udagramtinedev.c9enzdgp18lg.us-east-1.rds.amazonaws.com",
     "dialect": "postgres",
-    "aws_region": "us-east-2",
+    "aws_region": "us-east-1",
     "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_media_bucket": ""
   },
   "jwt": {
     "secret": " "
