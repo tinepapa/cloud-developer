7696d58fddbb1b309dc22773f9a6e98aa04b8380
tinepapa <tinembaye@hotmail.com>
2022-08-15 12:16:48 +0000

    Your Own Instagram on AWS

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -14,18 +14,22 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   app.use(bodyParser.json());
 
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+
+  app.get( "/filteredimage", async(req:express.Request, res:express.Response) => {
+    let {image_url} = req.query;
+    if (!image_url){
+      res.status(400).send('Error: The submitted url is empty');
+    } else {
+      await filterImageFromURL(image_url).then( function (image_filtered_path){
+        res.sendFile(image_filtered_path, () => {       
+          deleteLocalFiles([image_filtered_path]);       
+        });   
+      }).catch(function(err){
+        res.status(400).send('Error:' + err + 'Unfortunately the image cannot be filtered.');
+      });  
+
+    }
+  });
 
   /**************************************************************************** */
 
@@ -33,7 +37,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get( "/", async ( req:express.Request, res:express.Response ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
   
